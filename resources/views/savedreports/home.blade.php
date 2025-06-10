e9f86300718d698ce7828da2240a8f000ba017d0
Scott Ross <lsr2912@gmail.com>
2025-06-10 12:04:17 -0400

    home blade updated to no longer expect report_data

--- resources/views/savedreports/home.blade.php
+++ resources/views/savedreports/home.blade.php
@@ -10,15 +10,6 @@
 <v-main>
   <h1>{{ auth()->user()->name }}'s dashboard</h1>
   <h3 class="component-subhead">{{ $inst_count }} institution(s) and {{ $prov_count }} provider(s) connected</h3>
-  <div class="dashboard-section">
-    @if (sizeof($report_data) >= 1)
-      <h3 class="actionable-subhead">My Saved Reports</h3>
-    @else
-      <p>No Custom Reports</p>
-    @endif
-    <a class="btn v-btn v-btn--contained v-size--small section-action" href="/reports/create">Create a Report</a>
-    <home-saved-reports :reports="{{ json_encode($report_data) }}"></home-saved-reports>
-  </div>
   @if (sizeof($data_alerts) > 0)
     <div class="dashboard-section">
       <alert-summary-table :alerts="{{ json_encode($data_alerts) }}"></alert-summary-table>
