cb190ffd0051a3f05e2b83828a901bb93a5b6a43
Scott Ross <lsr2912@gmail.com>
2025-07-08 13:18:15 -0400

    Take out "include_component_details" from IR report requests.

--- app/Sushi.php
+++ app/Sushi.php
@@ -227,7 +227,7 @@ class Sushi extends Model
             $uri_atts  = "&attributes_to_show=Access_Method";
             $uri_atts .= ($release == "5") ? "%7CData_Type" : "";
         } elseif ($report->name == "IR") {
-            $uri_atts = "&Include_Parent_Details=True&Include_Component_Details=True";
+            $uri_atts = "&Include_Parent_Details=True";
             $uri_atts .= "&attributes_to_show=Access_Method%7CAccess_Type%7CData_Type%7CYOP";
             $uri_atts .= "%7CAuthors%7CPublication_Date%7CArticle_Version";
         }
