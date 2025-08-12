bffc453ee7c16cb3fd5da4007e526b11c02d57b1
Scott Ross <lsr2912@gmail.com>
2025-08-12 16:20:09 -0400

    Update middleware

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
 {
@@ -11,16 +13,16 @@ class CheckRole
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  \App\Role $roles
+     * @param  String $input_roles
      * @return mixed
      */
-    public function handle($request, Closure $next, ...$roles)
+    public function handle(Request $request, Closure $next, string $input_roles): Response
     {
         $user = \Auth::user();
         if ($user->hasRole("ServerAdmin")) {
             return $next($request);
         }
-
+        $roles = preg_split($input_roles,', ');
         foreach ($roles as $role) {
             if ($user->hasRole($role)) {
                 return $next($request);
