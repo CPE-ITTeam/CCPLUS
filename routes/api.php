a9fa0aa03c06ca40e78eea19fc82c4f925241017
Scott Ross <lsr2912@gmail.com>
2025-08-28 14:43:52 -0400

    Replace web routes with a catch-all and apply first update to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('logout', 'App\Http\Controllers\Auth\LoginController@logout');
+Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/user', function (Request $request) {
+       return $request->user();
+    });
+});
+//
+Route::get('consoList', 'App\Http\Controllers\ConsortiumController@index')->name('consoList');
