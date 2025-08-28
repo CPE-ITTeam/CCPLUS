b3eb6eac3880b250c84aef043e6ed9db03a7b6fa
Scott Ross <lsr2912@gmail.com>
2025-08-28 12:25:45 -0400

    Updates to composer, npm, and vite configs for moving to an SPA

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                api: 'modern-compiler', // or "modern"
+                api: 'modern-compiler',
                 silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import']
             }
         }
@@ -24,8 +23,8 @@ export default defineConfig({
         }),
         vuetify({ autoImport: true }),
         laravel({
-            input: [ 'resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+           input: ['resources/js/app.js'],
+           refresh: true,
         }),
     ],
 });
