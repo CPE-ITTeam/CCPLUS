4bce5859c502cee7dd432a8261685bc2ffb1f2f0
Scott Ross <lsr2912@gmail.com>
2025-08-28 12:39:45 -0400

    Remove Inertia from application bootstrap and add api setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,24 +4,24 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Http\Request;
-use \App\Http\Middleware\HandleInertiaRequests;
 use \App\Http\Middleware\AssignConsortiumDb;
 use \App\Http\Middleware\CheckRole;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
         $middleware->prepend([
             StartSession::class,
             AssignConsortiumDb::class
         ]);
 
         $middleware->web(append: [
-            HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
