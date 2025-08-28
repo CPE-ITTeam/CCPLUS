e85569bc3e7b01b6978fba9b279c1933aab6ee8c
Scott Ross <lsr2912@gmail.com>
2025-08-28 14:23:54 -0400

    Update/replace login and logout fucntions in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,111 +2,52 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
-use Hash;
+use App\Models\Consortium;
+use App\Models\UserRole;
 
-class LoginController extends Controller
+/*
+|-----------------------------------------------------------------------------------
+| Login Controller: 
+|     Only two methods here to support GET routes for /login and /logout.
+|
+| Most of authentication is in Fortify and app/Http/Providers/FortifyServiceProvider
+|-----------------------------------------------------------------------------------
+*/
+
+
+class LoginController extends BaseController
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
+   /**
+    * Login defined by routes/api.php
+    *
+    * @return \Illuminate\Http\Response
     */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login or logout.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
-
-    /** --lsr : 7/25/19--
-     * Override Foundation class login traits from
-     * Illuminate\Foundation\Auth\AuthenticatesUsers
-     * ---------------------------------------------
-     */
     public function login(Request $request)
     {
-        // AssignConsortiumDB middleware tests+sets the session variable
-        // so we'll be attempting against the right database
+        // Set the consortium handle before attempting to authenticate
+        $key = ($request->consortium == '') ? "con_template" : $request->consortium;
+        config(['database.connections.consodb.database' => 'ccplus_' . $key]);    
 
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (
-            method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)
-        ) {
-            $this->fireLockoutEvent($request);
-            return $this->sendLockoutResponse($request);
-        }
-
-        // Check for ServerAdmin credential
-        if (!is_null(config('ccplus.global_admin')) &&
-            !is_null(config('ccplus.global_admin_pass')) &&
-            $request->email == config('ccplus.global_admin') &&
-            Hash::check($request->password, config('ccplus.global_admin_pass'))
-           ) {
-            if ($this->attemptLogin($request)) {
-                return $this->sendLoginResponse($request);
-            }
-        }
-
-        if ($this->attemptLogin($request)) {
-            if (Auth::user()->is_active) {
-                return $this->sendLoginResponse($request);
-            } else {    // Credentials right, but account marked inactive
-                $this->incrementLoginAttempts($request);
-                return response()->json([ 'error' => 'This account is not active.'], 401);
-            }
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-        return $this->sendFailedLoginResponse($request);
-    }
-
-    protected function authenticated(Request $request, $user)
-    {
-        $user->last_login = now();
-        $user->save();
-        session()->flash("success","");
-        if ($user->hasRole('ServerAdmin')) {
-            return redirect("/server/home");
-        } else if ($user->hasRole('Admin')) {
-          return redirect("/consoadmin");
-        } else {
-            return redirect("/");
+        // Check credentials
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) { 
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
+            $success['user'] = $user;
+            $success['roles'] = $user->allRoles();
+            return $this->sendResponse($success, 'User successfully authenticated.');
+        } else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
         }
     }
 
     public function logout(Request $request) {
-        Auth::logout();
+        Auth::guard('web')->logout();
         session(['ccp_con_key' => '']);
         $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect('/login');
     }
 }
