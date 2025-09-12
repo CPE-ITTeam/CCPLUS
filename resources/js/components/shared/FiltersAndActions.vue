fef366db08d46ffed83d5c271ccf7c4b53ae4150
Scott Ross <lsr2912@gmail.com>
2025-09-12 17:09:56 -0400

    Add FiltersAndActions component

---
+++ resources/js/components/shared/FiltersAndActions.vue
@@ -0,0 +1,25 @@
+<!-- toolbar/FiltersAndActions -->
+<script setup>
+  const props = defineProps({ dataset: { type: String, required: true } });
+
+  const emit = defineEmits(['customAction', 'add']);
+  const roles = ['Admin', 'Editor', 'Viewer'];
+  const emitAction = (type, payload) => {
+    emit('customAction', { type, payload });
+  };
+</script>
+
+<template>
+  <!-- Example: dataset-specific filters -->
+  <v-col v-if="dataset === 'users'">
+    <v-select label="Role" :items="roles" @change="emitAction('filterRole', $event)" />
+  </v-col>
+
+  <!-- Add Button -->
+  <v-col cols="auto">
+    <v-btn color="primary" @click="$emit('add')">
+      <v-icon start>mdi-plus</v-icon>
+      Add
+    </v-btn>
+  </v-col>
+</template>
