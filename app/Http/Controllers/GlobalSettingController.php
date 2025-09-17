fd1e7315d18143d22512fd7e1d71309367711488
Scott Ross <lsr2912@gmail.com>
2025-09-17 17:16:28 -0400

    Update index and store methods for GlobalSettingController

--- app/Http/Controllers/GlobalSettingController.php
+++ app/Http/Controllers/GlobalSettingController.php
@@ -2,59 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use App\GlobalSetting;
+use App\Models\GlobalSetting;
 use Illuminate\Http\Request;
 use \Illuminate\Contracts\Cache\Factory;
 
 class GlobalSettingController extends Controller
 {
-  public function __construct()
-  {
-      $this->middleware(['auth','role:ServerAdmin']);
-  }
+    public function __construct()
+    {
+        $this->middleware(['auth','role:ServerAdmin']);
+    }
 
-  /**
-   * Index method for GlobalSetting Controller
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-      // Get settings as an associative array of name => value
-      $skip_vars = array('server_admin','server_admin_pass');
-      $settings = GlobalSetting::whereNotIn('name',$skip_vars)->pluck('value', 'name')->toArray();
-      return view('globalsettings.index', compact('settings'));
-  }
+    /**
+     * Settings method for GlobalAdmin Controller
+     * @param  String $type    // 'mail', 'config', or 'all'
+     * @return JSON
+     */
+    public function index($type)
+    {
+        // Get global settings, minus the server admin credentials
+        $skip_vars = array('server_admin','server_admin_pass','max_name_length');
+        $all_settings = GlobalSetting::whereNotIn('name',$skip_vars)->get();
+        $records = array();
+        foreach ($all_settings as $setting) {
+            if ($setting->type == $type || $type == 'all') {
+                $records[$setting->name] = $setting->value;
+            }
+        }
+        return response()->json(['records' => $records], 200);
+    }
 
-  /**
-   * Store / Replace ALL global setting variable values
-   * @param  \Illuminate\Http\Request  $request
-   * @return JSON
-   */
-  public function store(Request $request, Factory $cache)
-  {
-     // Validate form inputs
-      $this->validate($request, ['all_globals' => 'required']);
-      $input = $request->all();
+    /**
+     * Store / Replace global setting variable values
+     * @param  \Illuminate\Http\Request  $request
+     * @return JSON
+     */
+    public function store(Request $request, Factory $cache)
+    {
+        // Validate form inputs
+        $this->validate($request, ['settings' => 'required']);
+        $input = $request->all();
 
-      // Get all current settings
-      $settings = GlobalSetting::get();
-      if (!$settings) {
-          return response()->json(['result' => false, 'msg' => 'Error pulling current settings!!']);
-      }
+        // Get all current settings
+        $settings = GlobalSetting::get();
+        if (!$settings) {
+            return response()->json(['result' => false, 'msg' => 'Error pulling current settings!!']);
+        }
 
-      // update and save them all
-      foreach ($input['all_globals'] as $var) {
-          $setting = $settings->where('name', $var['name'])->first();
-          if ($setting) {
-              $setting->value = $var['value'];
-              $setting->save();
-          }
-      }
+        // Save all input values that have a (by-name) matching setting in the table
+        foreach ($input['settings'] as $key => $input) {
+            $setting = $settings->where('name', $key)->first();
+            if ($setting) {
+                $setting->value = $input;
+                $setting->save();
+            }
+        }
 
-      // Clear the 'ccplus' section of the cached configuration
-      $cache->forget('ccplus');
-      return response()->json(['result' => true, 'msg' => 'Settings successfully updated!']);
-  }
+        // Clear the 'ccplus' section of the cached configuration
+        $cache->forget('ccplus');
+        return response()->json(['result' => true, 'msg' => 'Settings successfully updated!']);
+    }
 
 }
