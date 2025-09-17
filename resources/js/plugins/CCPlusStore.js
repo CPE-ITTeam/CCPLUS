d67074f21b42b11c03652eaf7cfb0947ef3bee04
Scott Ross <lsr2912@gmail.com>
2025-09-17 17:21:26 -0400

    Update CCPlusStore to receive "records" from ConsortiaController. Also add static arrays for fiscalYr months and timeZones

--- resources/js/plugins/CCPlusStore.js
+++ resources/js/plugins/CCPlusStore.js
@@ -1,6 +1,7 @@
 // Pinia store : plugins/CCPlusStore.js
 import { defineStore } from 'pinia'
 import { useFetch } from '@vueuse/core';
+
 export const useCCPlusStore = defineStore('useCCPlusStore', {
   state: () => {
     return {
@@ -27,7 +28,7 @@ export const useCCPlusStore = defineStore('useCCPlusStore', {
     async getConsortia() {
       try {
         const { data } = await useFetch("/api/consoList").get().json();
-        this.consortia = [...data.value.consortia];
+        this.consortia = [...data.value.records];
         return { success: true }; // Return success status and data
       } catch (error) {
         return { success: false, error: 'Error fetching consortium list: '+error.message };
@@ -36,3 +37,31 @@ export const useCCPlusStore = defineStore('useCCPlusStore', {
 
   },
 });
+
+export const fyMonths = [
+  { label: 'January', value: '01' },
+  { label: 'February', value: '02' },
+  { label: 'March', value: '03' },
+  { label: 'April', value: '04' },
+  { label: 'May', value: '05' },
+  { label: 'June', value: '06' },
+  { label: 'July', value: '07' },
+  { label: 'August', value: '08' },
+  { label: 'September', value: '09' },
+  { label: 'October', value: '10' },
+  { label: 'November', value: '11' },
+  { label: 'December', value: '12' },
+];
+
+export const timeZones = [
+  'America/Los_Angeles',
+  'America/Denver',
+  'America/Chicago',
+  'America/New_York',
+  'UTC',
+  'Europe/Helsinki',
+  'Europe/Istanbul',
+  'Asia/Kolkata',
+  'Australia/Sydney'
+];
+  
