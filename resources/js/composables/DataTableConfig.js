fd178a4a1694baf6516eb6216a18877891872bb6
Scott Ross <lsr2912@gmail.com>
2025-09-18 16:32:24 -0400

    Add initial datatable configs for Harvests, HarvestQueue, and SavedReports

---
+++ resources/js/composables/DataTableConfig.js
@@ -0,0 +1,120 @@
+  // Dataset config map
+export const tableSetup = {
+    consortia: {
+      url: '',
+      headers: [
+        { title: 'Status', key: 'status' },
+        { title: 'Consortium Name', key: 'name' },
+        { title: 'Display Name', key: 'displayName' },
+        { title: 'DB Key', key: 'databaseKey' },
+        { title: 'Admin', key: 'admin' },
+        { title: 'Email', key: 'email' },
+      ],
+      searchFields: [ 'name', 'displayName', 'databaseKey', 'admin', 'email' ]
+    },
+    credentials: {
+      url: '/api/getCreds',
+      headers: [
+        { title: 'Connected', key: 'connected' },
+        { title: 'Name', key: 'provider.name' },
+        { title: 'Institution', key: 'institution.name' },
+        { title: 'PR', key: 'PR', class: 'narrow-col' },
+        { title: 'DR', key: 'DR', class: 'narrow-col' },
+        { title: 'TR', key: 'TR', class: 'narrow-col' },
+        { title: 'IR', key: 'IR', class: 'narrow-col' },
+      ],
+      searchFields: ['name', 'abbrev', 'dataHost']
+    },
+    institutions: {
+      url: '/api/getInsts',
+      headers: [
+        { title: 'Status', key: 'status' },
+        { title: 'Institution Name', key: 'name' },
+        { title: 'Type', key: 'type' },
+        { title: 'Consortium', key: 'consortiumKey' },
+      ],
+      searchFields: ['name', 'type', 'groups', 'consortiumKey']
+    },
+    institutionGroups: {
+      url: '/api/getInstGroups',
+      headers: [
+        { title: 'Group', key: 'name' },
+        { title: 'Member Count', key: 'count' },
+      ],
+      searchFields: ['name']
+    },
+    institutionTypes: {
+      url: '/api/getInstTypes',
+      headers: [
+        { title: 'Type Value', key: 'id' },
+        { title: 'Name', key: 'name' },
+      ],
+      searchFields: ['id', 'name']
+    },
+    platforms: {
+      url: '/api/getPlatforms',
+      headers: [
+        { title: 'Status', key: 'status' },
+        { title: 'Abbr', key: 'abbrev' },
+        { title: 'Rel', key: 'release' },
+        { title: 'Platform Name', key: 'name' },
+        { title: 'Data Host', key: 'dataHost' },
+        { title: 'Harvest On', key: 'harvestDate' },
+        { title: 'Conx', key: 'connectionCount' },
+        { title: 'Updated', key: 'datetime' },
+      ],
+      searchFields: ['name', 'abbrev', 'dataHost', 'registryId']
+    },
+    users: {
+      url: '/api/getUsers',
+      headers: [
+        { title: 'Status', key: 'status' },
+        { title: 'Email', key: 'email' },
+        { title: 'Institution', key: 'institution.name' },
+        { title: 'Consortium Key', key: 'consortiumKey' },
+        { title: 'Role', key: 'role_string' },
+        { title: 'Last Login', key: 'last_login' },
+      ],
+      searchFields: [ 'username', 'email', 'institution.name', 'consortiumKey', 'role' ]
+    },
+    harvests: {
+      url: '/api/getHarvests',
+      headers: [
+        { title: 'Result Date', key: 'updated' },
+        { title: 'Platform', key: 'prov_name' },
+        { title: 'Institution', key: 'inst_name' },
+        { title: 'Report', key: 'report_name', align: 'center' },
+        { title: 'Usage Date', key: 'yearmon' },
+        { title: 'Harvest ID', key: 'id', align: 'center', width: '100px'},
+        { title: 'Result', key: 'error.id' },
+        { title: 'Status', key: 'status', align: 'center'},
+      ],
+      searchFields: [ 'prov_name', 'inst_name', 'report_name', 'yearmon', 'status' ]
+    },
+    jobs: {
+      url: '/api/getJobs',
+      headers: [
+        { title: 'Created', key: 'created' },
+        { title: 'Platform', key: 'prov_name' },
+        { title: 'Institution', key: 'inst_name' },
+        { title: 'Report', key: 'report_name', align: 'center' },
+        { title: 'Usage Date', key: 'yearmon' },
+        { title: 'Harvest ID', key: 'id', align: 'center'},
+        { title: 'Status', key: 'dStatus' },
+      ],
+      searchFields: [ 'prov_name', 'inst_name', 'report_name', 'yearmon', 'dStatus' ]
+    },
+    savedreports: {
+      url: '/api/getSavedReports',
+      headers: [
+        { title: 'Report Title', key: 'title' },
+        { title: 'Report', key: 'master_name' },
+        { title: 'Date Range', key: 'date_range' },
+        { title: 'Last Run Date', key: 'last_harvest' },
+        { title: 'Format', key: 'format' },
+        { title: 'Include Zeros', key: 'exclude_zeros', align: 'center' },
+        { title: 'Actions', key: 'action', align: 'end', sortable: false },
+      ],
+      searchFields: [ 'title', 'master_name', 'date_range', 'last_harvest', 'format' ]
+    }
+};
