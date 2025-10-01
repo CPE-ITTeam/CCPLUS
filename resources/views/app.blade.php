1b3d8697f4e089b7858c34ac686d8755128e3b95
Scott Ross <lsr2912@gmail.com>
2025-10-01 15:44:24 -0400

    Base application view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CC-Plus Vue-3 Work-In-Progress</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css" rel="stylesheet">
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
