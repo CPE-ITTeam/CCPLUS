1c2a899f703ce36c4798fb7bc47234e71c50b5cf
Scott Ross <lsr2912@gmail.com>
2025-10-17 15:19:06 -0400

    Add wrapper for RolesTable

---
+++ resources/js/components/tables/RolesTable.vue
@@ -0,0 +1,8 @@
+<!-- tables/RolesTable.vue -->
+<script setup>
+  import DatasetViewer from '../shared/DatasetViewer.vue';
+</script>
+
+<template>
+  <DatasetViewer datasetKey="roles" />
+</template>
