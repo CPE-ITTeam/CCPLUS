d1dd7bb576311b7236beba4a3a4066328f21c9a8
Scott Ross <lsr2912@gmail.com>
2025-10-22 12:46:40 -0400

    Add connections datatable wrapper

---
+++ resources/js/components/tables/ConnectionsTable.vue
@@ -0,0 +1,8 @@
+<!-- views/ConnectionsTable.vue -->
+<script setup>
+  import DatasetViewer from '../shared/DatasetViewer.vue';
+</script>
+
+<template>
+  <DatasetViewer datasetKey="connections" />
+</template>
