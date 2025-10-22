bdced940e9430d8f0ea1eb1c630957b8da0e51a9
Scott Ross <lsr2912@gmail.com>
2025-10-22 12:54:15 -0400

    Remove old CredentialsTable wrapper

--- resources/js/components/tables/CredentialsTable.vue
+++
@@ -1,8 +0,0 @@
-<!-- views/Credentials.vue -->
-<script setup>
-  import DatasetViewer from '../shared/DatasetViewer.vue';
-</script>
-
-<template>
-  <DatasetViewer datasetKey="credentials" />
-</template>
