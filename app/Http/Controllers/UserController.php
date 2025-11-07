85f786e332833617e4fe416c9e12dafe95534220
Scott Ross <lsr2912@gmail.com>
2025-11-07 14:44:39 -0500

    Update index() method to return users for just the current instance and include filter_options for the datatable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,58 +34,41 @@ class UserController extends Controller
         $limit_to_insts = [];
         $server_admin = config('ccplus.server_admin');
 
-        // serverAdmin sees all users across all instances, otherwise return for current session DB
-        $original_db_key = session('ccp_con_key');
-        $cur_db_key = $original_db_key;
-        $where = ($thisUser->isServerAdmin()) ? array(['is_active',1]) : array(['ccp_key',$original_db_key]);
-        $instances = Consortium::where($where)->get(['id','ccp_key','name'])->toArray();
+        // Setup filtering options for the datatable
+        $filter_options = array('statuses' => array('ALL','Active','Inactive'));
+        $filter_options['roles'] = Role::where('name','<>','ServerAdmin')->get(['id','name'])->toArray();
 
-        foreach ($instances as $conso) {
-            $cur_db_key = $conso['ccp_key'];
-
-            // Switch conso assignment
-            if ($cur_db_key != $original_db_key) {
-                config(['database.connections.consodb.database' => "ccplus_" . $cur_db_key]);
-                session(['ccp_con_key' => $cur_db_key]);
-                DB::reconnect('consodb');
-            }
-
-            // Limit by institution based on users's role(s)
-            if (!$thisUser->isServerAdmin()) {
-                $limit_to_insts = $thisUser->adminInsts();
-                if ($limit_to_insts === [1]) $limit_to_insts = [];
-            }
-
-            // Pull user records - exclude serverAdmin
-            $user_data = User::with('roles','institution:id,name')
-                                ->when(count($limit_to_insts)>0, function ($qry) use ($limit_to_insts) {
-                                    return $qry->whereIn('inst_id', $limit_to_insts);
-                                })
-                                ->where('email', '<>', $server_admin)
-                                ->orderBy('name', 'ASC')->get();
-
-            // Make user role names one string, role IDs into an array, and status to a string for the view
-            foreach ($user_data as $rec) {
-                // exclude any users that cannot be managed by thisUser from the displayed list
-                if (!$rec->canManage()) continue;
-
-                // Setup array for this user data
-                $user = $rec->toArray();
-                $user['ccp_key'] = $cur_db_key;
-                $user['status'] = ($rec->is_active) ? "Active" : "Inactive";
-                $user['fiscalYr'] = ($rec->fiscalYr) ? $rec->fiscalYr : config('ccplus.fiscalYr');
-                $data[] = $user;                
-            }
+        // Limit by institution based on users's role(s)
+        if (!$thisUser->isServerAdmin()) {
+            $limit_to_insts = $thisUser->adminInsts();
+            if ($limit_to_insts === [1]) $limit_to_insts = [];
         }
 
-        // Reset the consodb setting if it was changed
-        if ($thisUser->isServerAdmin() && count($instances) > 0 && $cur_db_key != $original_db_key) {
-            config(['database.connections.consodb.database' => "ccplus_" . $original_db_key]);
-            session(['ccp_con_key' => $original_db_key]);
-            DB::reconnect('consodb');
+        // Pull user records - exclude serverAdmin
+        $user_data = User::with('roles','institution:id,name')
+                            ->when(count($limit_to_insts)>0, function ($qry) use ($limit_to_insts) {
+                                return $qry->whereIn('inst_id', $limit_to_insts);
+                            })
+                            ->where('email', '<>', $server_admin)
+                            ->orderBy('name', 'ASC')->get();
+
+        // Make user role names one string, role IDs into an array, and status to a string for the view
+        foreach ($user_data as $rec) {
+            // exclude any users that cannot be managed by thisUser from the displayed list
+            if (!$rec->canManage()) continue;
+
+            // Setup array for this user data
+            $user = $rec->toArray();
+            $user['status'] = ($rec->is_active) ? "Active" : "Inactive";
+            $user['fiscalYr'] = ($rec->fiscalYr) ? $rec->fiscalYr : config('ccplus.fiscalYr');
+            $data[] = $user;                
         }
 
-        return response()->json(['records' => $data, 'result' => true], 200);
+        // Add filtering options for institutions
+        $instIds = $user_data->pluck('inst_id')->toArray();
+        $filter_options['institutions'] = Institution::whereIn('id',$instIds)->get(['id','name'])->toArray();
+
+        return response()->json(['records' => $data, 'options' => $filter_options, 'result' => true], 200);
     }
 
     /**
