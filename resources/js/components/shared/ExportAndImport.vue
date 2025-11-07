4dc9b97aa1676139c4e2f597424d5186aed52942
Scott Ross <lsr2912@gmail.com>
2025-11-07 14:29:02 -0500

    Put buttons in columns and add the "Add" button to the template

--- resources/js/components/shared/ExportAndImport.vue
+++ resources/js/components/shared/ExportAndImport.vue
@@ -1,17 +1,27 @@
 <!-- toolbar/ExportAndImport -->
 
 <script setup>
-  defineEmits(['export', 'import']);
+  defineEmits(['export', 'import', 'add']);
 </script>
 
 <template>
-  <v-btn color="secondary" @click="$emit('export')">
+  <!-- Add Button -->
+  <v-col cols="auto">
+    <v-btn color="primary" @click="$emit('add')">
+    <v-icon start>mdi-plus</v-icon>
+      Add
+    </v-btn>
+  </v-col>
+  <v-col cols="auto">
+    <v-btn color="secondary" @click="$emit('export')">
     <v-icon start>mdi-export</v-icon>
-    Export
-  </v-btn>
-
-  <v-btn color="secondary" @click="$emit('import')">
+      Export
+    </v-btn>
+  </v-col>
+  <v-col cols="auto">
+    <v-btn color="secondary" @click="$emit('import')">
     <v-icon start>mdi-upload</v-icon>
-    Import
-  </v-btn>
+      Import
+    </v-btn>
+  </v-col>
 </template>
