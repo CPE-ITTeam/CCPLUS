e903ed639a5c717163f96025be20bfc572a2c6ce
Scott Ross <lsr2912@gmail.com>
2025-11-07 14:27:50 -0500

    Updates to use DataTableConfig definitions / changes to props (Add button now lives in the ExportAndImport  component)

--- resources/js/components/shared/FiltersAndActions.vue
+++ resources/js/components/shared/FiltersAndActions.vue
@@ -1,25 +1,38 @@
 <!-- toolbar/FiltersAndActions -->
 <script setup>
-  const props = defineProps({ dataset: { type: String, required: true } });
+  const props = defineProps({
+    filters: { type: Object, required: true },
+  });
+  var filterModel;
 
-  const emit = defineEmits(['customAction', 'add']);
-  const roles = ['Admin', 'Editor', 'Viewer'];
-  const emitAction = (type, payload) => {
-    emit('customAction', { type, payload });
+//ToDo:: emit/change filter action needs to pass changes to parent component
+
+  const emit = defineEmits(['customAction']);
+
+  const emitAction = (filter, payload) => {
+    emit('customAction', { filter, payload });
   };
 </script>
 
 <template>
-  <!-- Example: dataset-specific filters -->
-  <v-col v-if="dataset === 'users'">
-    <v-select label="Role" :items="roles" @change="emitAction('filterRole', $event)" />
-  </v-col>
+  <v-col v-for="(filter,idx) in props.filters" :key="idx" :cols="(typeof(filter.cols)=='undefined') ? 2 : filter.cols">
 
-  <!-- Add Button -->
-  <v-col cols="auto">
-    <v-btn color="primary" @click="$emit('add')">
-      <v-icon start>mdi-plus</v-icon>
-      Add
-    </v-btn>
+    <!-- AutoComplete - multiple select -->
+    <v-autocomplete v-if="filter.type=='mselect'" v-model="filterModel" :label="filter.label" multiple
+        :items="filter.items" :item-title="filter.txt" :item-value="filter.val"
+        @change="emitAction('filter.label', $event)" />
+
+    <!-- AutoComplete - single item select -->
+    <v-autocomplete v-if="filter.type=='select'" v-model="filterModel" :label="filter.label" :items="filter.items"
+        :item-title="filter.txt" :item-value="filter.val" @change="emitAction('filter.name', $event)" />
+
+    <!-- AutoComplete - return single object -- needed anywhere??
+    <v-autocomplete v-if="filter.type=='select'" v-model="filterModel" :label="filter.label" return-object
+        :items="props.options[filter.optionsKey]" :item-title="filter.txt" :item-value="filter.val"
+        @change="emitAction('filter.name', $event)" />
+    -->
+    <!-- Return a Single Scalar value -->
+    <v-select v-if="filter.type=='text'" v-model="filterModel" :label="filter.label" :items="filter.items" 
+             :item-title="filter.txt" :item-value="filter.val" @change="emitAction('filter.name', $event)" />
   </v-col>
 </template>
