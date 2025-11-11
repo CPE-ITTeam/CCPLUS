3a1e0b7497563b3e24de0addd336eae0b6f52059
Scott Ross <lsr2912@gmail.com>
2025-11-11 17:41:44 -0500

    Update wrapper to catch and echo update:conso event to parent

--- resources/js/components/tables/InstitutionsTable.vue
+++ resources/js/components/tables/InstitutionsTable.vue
@@ -1,8 +1,9 @@
 <!-- tables/Institutions.vue -->
 <script setup>
   import DatasetViewer from '../shared/DatasetViewer.vue';
+  const emit = defineEmits(['update:conso']);
 </script>
 
 <template>
-  <DatasetViewer datasetKey="institutions" />
+  <DatasetViewer datasetKey="institutions" @update:conso="$emit('update:conso', $event)" />
 </template>
