06b70c873938d1979fdfbab366dd97694d0aa022
Scott Ross <lsr2912@gmail.com>
2025-11-19 15:33:36 -0500

    Add password confirmation and fix hide/reveal icons; minor layout changes; changes to select and combobox to use optTXT abd optVal from config composable

--- resources/js/components/shared/DataForm.vue
+++ resources/js/components/shared/DataForm.vue
@@ -17,6 +17,8 @@
     schema: { type: Object, required: true },
   });
   const formRef = ref();
+  var pw_show = ref(false);
+  var pwc_show = ref(false);
   const formValues = ref({...props.initialValues});
   const requiredRule = (v) => !!v || 'This field is required';
 
@@ -30,6 +32,10 @@
 
   const fieldLabels = computed(() => {
     return props.schema.fields.map(f => f.label);
+  });
+  const showConfirm = computed(() => {
+    return (Object.keys(formValues.value).includes('password'))
+           ? (formValues.value['password'].length > 0) : false;
   });
 
   // defineEmits(['submit', 'cancel']);
@@ -75,10 +81,8 @@
           <v-col v-if="!field.static" cols="12" class="d-flex ma-0 pa-0">
             <!-- Read-only display -->
             <div v-if="!field.editable || field.renderAsText">
-              <strong>{{ field.label }}:</strong>
-              <div class="text-medium-emphasis">
-                {{ formValues[field.name] }}
-              </div>
+              <strong>{{ field.label }}:</strong> &nbsp;
+              <span class="text-medium-emphasis">{{ formValues[field.name] }}</span>
             </div>
 
             <!-- ToggleIcon input -->
@@ -86,11 +90,15 @@
                         :toggleable="isToggleEditable(formValues[field.name])" :statusMap="statusMap"
                         :size="36" @update:modelValue="val => formValues[field.name] = val" />
                         
-            <!-- Password input -->
+            <!-- Password inputs -->
             <v-text-field v-else-if="field.type==='password'" v-model="formValues[field.name]" :label="field.label"
                           :type="pw_show ? 'text' : 'password'" :append-icon="pw_show ? 'mdi-eye' : 'mdi-eye-off'"
                           @click:append="pw_show = !pw_show" variant="outlined" :hint="field.helperText"
                           persistent-hint :rules="field.required ? [requiredRule] : []" density="compact"/>
+            <v-text-field v-else-if="field.type==='passconf' && showConfirm" :label="field.label" density="compact"
+                          v-model="formValues[field.name]" :hint="field.helperText" variant="outlined" persistent-hint
+                          :type="pwc_show ? 'text' : 'password'" :append-icon="pwc_show ? 'mdi-eye' : 'mdi-eye-off'"
+                          @click:append="pwc_show=!pwc_show" :rules="field.required ? [requiredRule] : []"/>
 
             <!-- Textarea -->
             <v-textarea v-else-if="field.type==='textarea'" v-model="formValues[field.name]" :label="field.label"
@@ -99,18 +107,22 @@
 
             <!-- Select inputs -->
             <v-select v-else-if="field.type==='select'" v-model="formValues[field.name]" :label="field.label"
-                      :items="schema.options[field.name].items" item-title="name" item-value="id" variant="outlined"
-                      :prepend-icon="field.icon" :hint="field.helperText" persistent-hint
+                      :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
+                      :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined"
                       :rules="field.required ? [requiredRule] : []" density="compact"/>
+            <v-select v-else-if="field.type==='selectObj'" v-model="formValues[field.name]" :label="field.label"
+                      :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
+                      :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined"
+                      return-object :rules="field.required ? [requiredRule] : []" density="compact"/>
             <v-combobox v-else-if="field.type==='mselect'" v-model="formValues[field.name]" :label="field.label"
-                        :items="schema.options[field.name].items" item-title="name" item-value="id" multiple
-                        persistent-hint :prepend-icon="field.icon" :hint="field.helperText" variant="outlined"
-                        :rules="field.required ? [requiredRule] : []" density="compact"/>
-          
-                      
+                      :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal"
+                      :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined" multiple
+                      :rules="field.required ? [requiredRule] : []" density="compact"/>
+
             <!-- Fallback text input -->
-            <v-text-field v-else v-model="formValues[field.name]" :label="field.label" variant="outlined"
-                          :prepend-icon="field.icon" :hint="field.helperText" persistent-hint density="compact"/>
+            <v-text-field v-else-if="field.type!='passconf'" v-model="formValues[field.name]" :label="field.label"
+                          variant="outlined":prepend-icon="field.icon" :hint="field.helperText" persistent-hint
+                          density="compact"/>
           </v-col>
         </v-row>
 
