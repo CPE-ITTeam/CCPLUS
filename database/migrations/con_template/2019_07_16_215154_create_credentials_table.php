1d3dd7b37d2f06873c41cbbbbd5e2e1d4470ed63
Scott Ross <lsr2912@gmail.com>
2025-11-21 12:47:45 -0500

    Fix class name for migration

--- database/migrations/con_template/2019_07_16_215154_create_credentials_table.php
+++ database/migrations/con_template/2019_07_16_215154_create_credentials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSushiSettingsTable extends Migration
+class CreateCredentialsTable extends Migration
 {
     /**
      * Run the migrations.
