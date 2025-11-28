df690f597d405ed946b15d1f125b7ca6107e88e0
Scott Ross <lsr2912@gmail.com>
2025-11-28 15:56:02 -0500

    Updates to handle filters; enable store+destroy routes; schema customizing by-datasetKey; add "visible" property to allow for "hiding" fields in Add dialogs; formSubmit() now handles update and store requests

--- resources/js/components/shared/DatasetViewer.vue
+++ resources/js/components/shared/DatasetViewer.vue
@@ -10,7 +10,8 @@
 
   const authStore = useAuthStore();
   const is_admin = authStore.is_admin;
-  const { ccGet, ccPatch, setConsoKey } = useAuthStore();
+  const is_conso_admin = authStore.is_conso_admin;
+  const { ccGet, ccPost, ccPatch, ccDestroy, setConsoKey } = useAuthStore();
   var consoKey = ref(authStore.ccp_key);  
   const props = defineProps({
     datasetKey: { type: String, required: true }
@@ -34,7 +35,7 @@
   const dialogOpen = ref(false);
   const editingItem = ref(null);
   const editableFields = ref([]);
-  const updateUrl = ref('');
+  const urlRoot = ref('');
   var itemOptions = {};
   var success = ref('');
   var failure = ref('');
@@ -44,8 +45,17 @@
     return (editingItem.value && config && editableFields.value.length>0);
   });
 
+  const toolbarFilters = computed(() => {
+    return Object.fromEntries(
+      Object.entries(filterOptions).filter(([key,val]) => filterOptions[key]['show'])
+    )
+  });
+
   const formSchema = computed(() => {
     const config = datasetConfig[props.datasetKey];
+    if (props.datasetKey=='roles') {
+      filterOptions.role.items = filterOptions.role.items.filter(r => !r.name.includes('Consortium'))
+    }
     return {
       type: dialogType.value,
       fields: [...config.fields],
@@ -75,8 +85,8 @@
     headers.value = [{ title: "", key: "" }];
     config.fields.forEach( (fld, idx) => {
       if (fld.header) headers.value.push({title: fld.label, key: fld.name});
-      config.fields[idx].static = (typeof(config.static)!='undefined') ?
-                                    (config.static.indexOf(fld.name) > -1) : false;
+      if (fld.name == 'conso' && !is_conso_admin) return;
+      config.fields[idx].static = config.static.includes(fld.name);
       // Set filterOptions for select(s) and toggle
       if ( (fld.type == 'select' || fld.type == 'mselect' || fld.type == 'selectObj' || fld.type == 'toggle') &&
            !config.fields[idx].static && fld.options == 'fromURL' &&
@@ -86,17 +96,22 @@
           'name': fld.name, 'label': fld.label, 'type': fld.type, 'val': fld.optVal, 'txt': fld.optTxt,
           'show': fld.isFilter, 'col': fld.filterCol, 'items': [...itemOptions[fld.name]], 'value': initVal
         };
+      } else if (Array.isArray(fld.options)) {
+        filterOptions[fld.name] = {
+          'name': fld.name, 'label': fld.label, 'type': fld.type, 'val': fld.optVal, 'txt': fld.optTxt,
+          'show': fld.isFilter, 'col': fld.filterCol, 'items': [...fld.options], 'value': fld.options[0]
+        };
       } else if ( fld.name == 'fiscalYr') {
         filterOptions['fiscalYr'] = {
           'name': fld.name, 'label': fld.label, 'type': fld.type, 'val': fld.optVal, 'txt': fld.optTxt,
-          'items': [...fyMonths], 'value': null
+          'show': true, 'items': [...fyMonths], 'value': null
         };
       }
     });
     // Set arrays for searchable and editable fields
     searchFields.value = config.fields.filter(fld => fld.searchable).map(f => f.name);
     editableFields.value = config.fields.filter(fld => fld.editable).map(f => f.name);
-    updateUrl.value = config.urlRoot+'/update';
+    urlRoot.value = config.urlRoot;
   }
 
   function handleToggle(value) {
@@ -122,6 +137,7 @@
         if (fld.name == 'institutions') item['institutions'] = item.inst_id;
         if (fld.name == 'roles') item['roles'] = item.role;
       }
+      config.fields[idx]['visible'] = true;
     });
     editingItem.value = {...item};
     dialogOpen.value = true;
@@ -132,8 +148,15 @@
     dialogType.value = "Add";
     dialogTitle = "Add New "+config.dialogTitle;
     editingItem.value = {};
-    config.required.forEach( key => {
-      editingItem[key] = null;
+    config.fields.forEach( (fld, idx) => {
+      // Skip fields not required for Add
+      if (!config.required.includes(fld.name)) return;
+      if (typeof(filterOptions[fld.name])!='undefined') {
+        editingItem.value[fld.name] = (filterOptions[fld.name].value) ? filterOptions[fld.name].value : null;
+      } else {
+        editingItem.value[fld.name] = null;
+      }
+      config.fields[idx]['visible'] = true;
     });
     dialogOpen.value = true;
   }
@@ -145,6 +168,7 @@
     var filterResult = [...allItems];
     for (const key of Object.keys(filterOptions)) {
       const filter = filterOptions[key]; 
+      if (!filter.show) continue;
       // If the result set is empty, just bail
       if (filterResult.length==0) break;
 
@@ -177,22 +201,21 @@ console.log('Filter by selectObj still needs work');
     dtKey.value++;
   }
 
-  async function handleDelete(item) {
-    const config = datasetConfig[props.datasetKey];
-    let destroyUrl = config.urlRoot+'/delete/'+editingItem.value.id;
+  async function handleDelete(id) {
+    let destroyUrl = urlRoot.value+'/delete/'+id;
     try {
-      const response = await ccPatch(destroyUrl);
+      const response = await ccDestroy(destroyUrl);
       if (response.result) {
-        allItems.splice(allItems.findIndex( ii => ii.id == editingItem.value.id),1);
-        filteredItems.splice(filteredItems.findIndex( ii => ii.id == editingItem.value.id),1);
+        allItems.splice(allItems.findIndex( ii => ii.id == id),1);
+        filteredItems.splice(filteredItems.findIndex( ii => ii.id == id),1);
         success.value = response.msg
+        dtKey.value++;
       } else {
         failure.value = response.msg
       }
     } catch (error) {
       console.error('Error deleting:', error);
     }
-    editingItem.value = null;
   }
 
   async function handleStatusUpdate(id, value) {
@@ -201,7 +224,7 @@ console.log('Filter by selectObj still needs work');
       let _item = Object.assign({}, allItems[_idx])
       _item['is_active'] = (value == 'Active') ? 1 : 0;
       try {
-        let url = updateUrl.value+'/'+id;
+        let url = urlRoot.value+'update/'+id;
         const response = await ccPatch(url, {'is_active': _item['is_active']});
         if (response.result) {
           _item['status'] = value;
@@ -219,22 +242,39 @@ console.log('Filter by selectObj still needs work');
   }
 
   async function handleFormSubmit(updatedValues) {
-    try {
-      let url = updateUrl.value+'/'+editingItem.value.id;
-      const response = await ccPatch(url, updatedValues);
-      if (response.result) {
-        let _idx = allItems.findIndex(ii => ii.id == editingItem.value.id);
-        allItems.splice(_idx,1,response.record);
-        _idx = allItems.findIndex(ii => ii.id == editingItem.value.id);
-        if (_idx >= 0) filteredItems.splice(_idx,1,response.record);
-        success.value = response.msg
-        dtKey.value++;
-      } else {
-        failure.value = response.msg
+    if (dialogType.value=='Edit') {
+      try {
+        let url = urlRoot.value+'update/'+editingItem.value.id;
+        const response = await ccPatch(url, updatedValues);
+        if (response.result) {
+          let _idx = allItems.findIndex(ii => ii.id == editingItem.value.id);
+          allItems.splice(_idx,1,response.record);
+          _idx = allItems.findIndex(ii => ii.id == editingItem.value.id);
+          if (_idx >= 0) filteredItems.splice(_idx,1,response.record);
+          success.value = response.msg
+          dtKey.value++;
+        } else {
+          failure.value = response.msg
+        }
+      } catch (error) {
+        console.error('Error updating:', error);
       }
-    } catch (error) {
-      console.error('Error updating:', error);
+    } else if (dialogType.value=='Add') {
+      try {
+        let url =  urlRoot.value+'/store';
+        const response = await ccPost(url, updatedValues);
+        if (response.result) {
+          allItems.push(response.record);
+          success.value = response.msg
+          dtKey.value++;
+        } else {
+          failure.value = response.msg
+        }
+      } catch (error) {
+        console.error('Error adding:', error);
+      }
     }
+    updateItems();
     dialogOpen.value = false;
     editingItem.value = null;
   }
@@ -252,7 +292,7 @@ console.log('Filter by selectObj still needs work');
 -->
 <template>
   <v-sheet>
-    <DataToolbar v-model="filterOptions" :search="search" :showSelectedOnly="showSelectedOnly" :dataset="props.datasetKey"
+    <DataToolbar v-model="toolbarFilters" :search="search" :showSelectedOnly="showSelectedOnly" :dataset="props.datasetKey"
                  @update:search="search = $event" @setFilter="updateItems" @update:showSelectedOnly="handleToggle"
                  @add="handleAddItem" @updateConso="handleChangeConso" />
 
