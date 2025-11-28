5bec04eaf8b7d5a615a19f0600c12d1432e487c9
Scott Ross <lsr2912@gmail.com>
2025-11-28 16:00:01 -0500

    Add toggle-initiated visibility control (for conso/inst/group interactions)

--- resources/js/components/shared/DataForm.vue
+++ resources/js/components/shared/DataForm.vue
@@ -1,6 +1,6 @@
 <!-- components/shared/DataForm.vue -->
 <script setup>
-  import { computed, ref } from 'vue';
+  import { computed, ref, reactive } from 'vue';
   import ToggleIcon from './ToggleIcon.vue';
 
   const props = defineProps({
@@ -19,24 +19,26 @@
   const formRef = ref();
   var pw_show = ref(false);
   var pwc_show = ref(false);
-  const formValues = ref({...props.initialValues});
   const requiredRule = (v) => !!v || 'This field is required';
   const opType = ref(props.schema.type);
+  var consoFlag = ref(false);
+  const m_schema = reactive({...props.schema});
+  const formValues = reactive({...props.initialValues});
 
   const fieldTypes = computed(() => {
-    return props.schema.fields.filter(fld => fld.searchable).map(f => f.type);
+    return m_schema.fields.filter(fld => fld.searchable).map(f => f.type);
   });
 
   const editableKeys = computed(() => {
-    return props.schema.fields.filter(fld => fld.editable).map(f => f.type);
+    return m_schema.fields.filter(fld => fld.editable).map(f => f.type);
   });
 
   const fieldLabels = computed(() => {
-    return props.schema.fields.map(f => f.label);
+    return m_schema.fields.map(f => f.label);
   });
   const showConfirm = computed(() => {
-    return (Object.keys(formValues.value).includes('password'))
-           ? (formValues.value['password'].length > 0) : false;
+    return (Object.keys(formValues).includes('password'))
+           ? (formValues['password'].length > 0) : false;
   });
 
   // defineEmits(['submit', 'cancel']);
@@ -44,8 +46,8 @@
 
   function isToggleField(field) {
     return field.type === 'toggle' ||
-      typeof formValues.value[field.name] === 'boolean' ||
-      isComplexToggle(formValues.value[field.name]);
+      typeof formValues[field.name] === 'boolean' ||
+      isComplexToggle(formValues[field.name]);
   }
 
   function isComplexToggle(value) {
@@ -60,6 +62,32 @@
     return true;
   }
 
+  // toggle-specific changes
+  function toggleChanged(fieldName) {
+    if (fieldName=='conso') {
+      consoFlag.value = !consoFlag.value;
+      let _idx = m_schema.fields.findIndex(f => f.name=='institution') ;
+      if (_idx >= 0) m_schema.fields[_idx]['visible'] = !consoFlag.value;
+      if (typeof(formValues['institution']) != 'undefined') formValues['institution'] = (consoFlag.value) ? 1 : null;
+      _idx = m_schema.fields.findIndex(f => f.name=='group') ;
+      if (_idx >= 0) m_schema.fields[_idx]['visible'] = !consoFlag.value;
+      if (typeof(formValues['group']) != 'undefined') formValues['group'] = null;
+    }
+  }
+
+  // select-specific changes
+  function selectChanged(fieldName) {
+    if (fieldName=='institution') {
+      let _idx = m_schema.fields.findIndex(f => f.name=='group') ;
+      if (_idx >= 0) m_schema.fields[_idx]['visible'] = false;
+      if (typeof(formValues['group']) != 'undefined') formValues['group'] = null;
+    } else if (fieldName=='group') {
+      let _idx = m_schema.fields.findIndex(f => f.name=='institution') ;
+      if (_idx >= 0) m_schema.fields[_idx]['visible'] = false;
+      if (typeof(formValues['institution']) != 'undefined') formValues['institution'] = null;
+    }
+  }
+
   const statusMap = {
     true: { icon: 'mdi-check-circle', color: 'green' },
     false: { icon: 'mdi-close-circle', color: 'red' },
@@ -69,16 +97,16 @@
 
   function submitForm() {
     if (formRef.value?.validate()) {
-      emit('submit', formValues.value);
+      emit('submit', formValues);
     }
   }
 </script>
 
 <template>
   <div>
-    <v-form v-if="props.schema.fields.length" ref="formRef" @submit.prevent="submitForm">
+    <v-form v-if="m_schema.fields.length" ref="formRef" @submit.prevent="submitForm">
       <v-container>
-        <v-row v-for="field in props.schema.fields" :key="field.name" class="mb-3">
+        <v-row v-for="field in m_schema.fields" :key="field.name" class="mb-3">
           <v-col v-if="!field.static && (opType=='Edit' || field.editable)" cols="12" class="d-flex ma-0 pa-0">
             <!-- Read-only display -->
             <div v-if="opType!='Add' && (!field.editable || field.renderAsText)">
@@ -87,9 +115,11 @@
             </div>
 
             <!-- ToggleIcon input -->
-            <ToggleIcon v-else-if="isToggleField(field)" v-model="formValues[field.name]"
-                        :toggleable="isToggleEditable(formValues[field.name])" :statusMap="statusMap"
-                        :size="36" @update:modelValue="val => formValues[field.name] = val" />
+            <div v-else-if="isToggleField(field)">
+              <strong>{{ field.label }} : </strong> &nbsp;
+              <ToggleIcon v-model="formValues[field.name]" :toggleable="isToggleEditable(formValues[field.name])"
+                          :statusMap="statusMap" :size="36" @update:modelValue="toggleChanged(field.name)" />
+            </div>
                         
             <!-- Password inputs -->
             <v-text-field v-else-if="field.type==='password'" v-model="formValues[field.name]" :label="field.label"
@@ -107,10 +137,12 @@
                         :hint="field.helperText" persistent-hint density="compact"/>
 
             <!-- Select inputs -->
-            <v-select v-else-if="field.type==='select'" v-model="formValues[field.name]" :label="field.label"
+            <v-select v-else-if="field.type==='select' && field.visible" v-model="formValues[field.name]"
+                      :label="field.label"
                       :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
                       :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined"
-                      :rules="field.required ? [requiredRule] : []" density="compact"/>
+                      :rules="field.required ? [requiredRule] : []" density="compact"
+                      @update:modelValue="selectChanged(field.name)" />
             <v-select v-else-if="field.type==='selectObj'" v-model="formValues[field.name]" :label="field.label"
                       :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
                       :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined"
@@ -121,9 +153,9 @@
                       :rules="field.required ? [requiredRule] : []" density="compact"/>
 
             <!-- Fallback text input -->
-            <v-text-field v-else-if="field.type!='passconf'" v-model="formValues[field.name]" :label="field.label"
-                          variant="outlined":prepend-icon="field.icon" :hint="field.helperText" persistent-hint
-                          density="compact"/>
+            <v-text-field v-else-if="field.type!='passconf' && field.visible" v-model="formValues[field.name]"
+                          :label="field.label" variant="outlined":prepend-icon="field.icon" :hint="field.helperText"
+                          persistent-hint density="compact"/>
           </v-col>
         </v-row>
 
