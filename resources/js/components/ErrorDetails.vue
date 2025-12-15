5030ba4453d8573a25cb2198e5b464387f49114b
Scott Ross <lsr2912@gmail.com>
2025-12-15 16:00:45 -0500

    Updated to handle unknown error codes

--- resources/js/components/ErrorDetails.vue
+++ resources/js/components/ErrorDetails.vue
@@ -6,42 +6,54 @@
       </v-icon>
     </v-card-actions>
     <v-card-title>
-      <span v-if="error_data.id<9000">COUNTER Error : {{ error_data.id }}</span>
+      <span v-if="!error_data.known_error">Platform-Specific Error Reported : {{ error_data.id }}</span>
+      <span v-else-if="error_data.id<9000">COUNTER Error : {{ error_data.id }}</span>
       <span v-else>CC-Plus Error : {{ error_data.id }}</span>
       <v-spacer></v-spacer>
     </v-card-title>
     <v-card-text>
       <v-container grid-list-md>
-        <v-row class="d-flex mb-2" no-gutters>
-          <v-col class="d-flex">{{ error_data.message }}</v-col>
-        </v-row>
-        <v-row class="d-flex mb-1" no-gutters>
-          <v-col class="d-flex pa-0" cols="3"><strong>Details:</strong></v-col>
-          <v-col class="d-flex px-4" cols="9">{{ error_data.explanation }}</v-col>
-        </v-row>
-        <v-row class="d-flex mb-1" no-gutters>
-          <v-col class="d-flex pa-0" cols="3"><strong>Suggestion:</strong></v-col>
-          <v-col class="d-flex px-4" cols="9">{{ error_data.suggestion }}</v-col>
-        </v-row>
-        <v-row v-if="error_data.detail.length>0" class="d-flex mb-1" no-gutters>
-          <v-col class="d-flex pa-0" cols="3">
-            <strong>{{ error_data.process_step }} Step:</strong>
-          </v-col>
-          <v-col class="d-flex px-4" cols="9">{{ error_data.detail }}</v-col>
-        </v-row>
-        <v-row v-if="error_data.help_url.length>0" class="d-flex mb-1" no-gutters>
-          <v-col class="d-flex pa-0" cols="3"><strong>Help URL:</strong></v-col>
-          <v-col class="d-flex px-4" cols="9">
-            <a :href="error_data.help_url" target="_blank">{{ error_data.help_url }}</a>
-          </v-col>
-        </v-row>
-        <v-row v-if="error_data.id<9000" class="d-flex mb-1" no-gutters>
-          <v-col class="d-flex pa-0">
-            <a href="#" @click="goCOUNTER(error_data.id, error_data.counter_url)">
-              Open COUNTER Details <v-icon>mdi-open-in-new</v-icon>
-            </a>
-          </v-col>
-        </v-row>
+        <div v-if="error_data.known_error">
+          <v-row class="d-flex mb-2" no-gutters>
+            <v-col class="d-flex">{{ error_data.message }}</v-col>
+          </v-row>
+          <v-row class="d-flex mb-1" no-gutters>
+            <v-col class="d-flex pa-0" cols="3"><strong>Details:</strong></v-col>
+            <v-col class="d-flex px-4" cols="9">{{ error_data.explanation }}</v-col>
+          </v-row>
+          <v-row class="d-flex mb-1" no-gutters>
+            <v-col class="d-flex pa-0" cols="3"><strong>Suggestion:</strong></v-col>
+            <v-col class="d-flex px-4" cols="9">{{ error_data.suggestion }}</v-col>
+          </v-row>
+          <v-row v-if="error_data.detail.length>0" class="d-flex mb-1" no-gutters>
+            <v-col class="d-flex pa-0" cols="3">
+              <strong>{{ error_data.process_step }} Step:</strong>
+            </v-col>
+            <v-col class="d-flex px-4" cols="9">{{ error_data.detail }}</v-col>
+          </v-row>
+          <v-row v-if="error_data.help_url.length>0" class="d-flex mb-1" no-gutters>
+            <v-col class="d-flex pa-0" cols="3"><strong>Help URL:</strong></v-col>
+            <v-col class="d-flex px-4" cols="9">
+              <a :href="error_data.help_url" target="_blank">{{ error_data.help_url }}</a>
+            </v-col>
+          </v-row>
+          <v-row v-if="error_data.id<9000" class="d-flex mb-1" no-gutters>
+            <v-col class="d-flex pa-0">
+              <a href="#" @click="goCOUNTER(error_data.id, error_data.counter_url)">
+                Open COUNTER Details <v-icon>mdi-open-in-new</v-icon>
+              </a>
+            </v-col>
+          </v-row>
+        </div>
+        <div v-else>
+          <v-row class="d-flex mb-2" no-gutters>
+            <p>
+              This error code is not a recognized COUNTER or CC-Plus error. There may be more detailed information
+              in the downloaded JSON data. Re-running the harvest manually (using the barley icon) could also help
+              troubleshoot the cause.
+            </p>
+          </v-row>
+        </div>
       </v-container>
     </v-card-text>
   </v-card>
