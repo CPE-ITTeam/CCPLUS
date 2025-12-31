5538ba2a3d13bdd44f5fba0c82268dde26f0b656
Scott Ross <lsr2912@gmail.com>
2025-12-31 15:11:30 -0500

    Add datatable wrapper for credentials (renamed from connections)

---
+++ resources/js/components/tables/CredentialsTable.vue
@@ -0,0 +1,9 @@
+<!-- views/CredentialsTable.vue -->
+<script setup>
+  import DatasetViewer from '../shared/DatasetViewer.vue';
+  const emit = defineEmits(['updateConso']);
+</script>
+
+<template>
+  <DatasetViewer datasetKey="credentials" @updateConso="$emit('updateConso', $event)" />
+</template>
