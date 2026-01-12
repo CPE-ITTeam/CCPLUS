d9cd19c99bebf256aeff84bf2ef25063089fcfd8
Scott Ross <lsr2912@gmail.com>
2026-01-12 15:35:07 -0500

    Update middleware to purge and reconnect consodb connection if it differs from the session (based on ccp_key)

--- app/Http/Middleware/AssignConsortiumDb.php
+++ app/Http/Middleware/AssignConsortiumDb.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use DB;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,9 +18,19 @@ class AssignConsortiumDb
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $session_key = $request->session()->get('ccp_key');
+        $session_key = session('ccp_key');
         $key = ($session_key=='') ? "con_template" : $session_key;
-        config(['database.connections.consodb.database' => 'ccplus_' . $key]);
+
+        // If the session key-derived database setting is different than
+        // the current assignment, purge and reconnect 
+        $orig_cnx = config('database.connections.consodb.database');
+        $sess_key = 'ccplus_' . $key;   
+        if ($sess_key != $orig_cnx) {
+            config(['database.connections.consodb.database' => $sess_key]);
+            DB::purge('consodb');
+            DB::reconnect('consodb');
+        }
+
         return $next($request);
     }
 }
