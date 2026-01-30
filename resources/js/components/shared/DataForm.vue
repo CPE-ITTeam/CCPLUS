86a36df7e9fecce2f054a250be1d6fb335a52dba
Scott Ross <lsr2912@gmail.com>
2026-01-30 14:22:18 -0500

    Changes to ensure m_schema is a deep, mutable copy of input prop. Changes to selectChanged(), submitForm() and (new) onMounted() functions to get institutions and platforms select options to respond to each other based on  unset values

--- resources/js/components/shared/DataForm.vue
+++ resources/js/components/shared/DataForm.vue
@@ -1,6 +1,6 @@
 <!-- components/shared/DataForm.vue -->
 <script setup>
-  import { computed, ref, reactive } from 'vue';
+  import { computed, ref, reactive, onMounted } from 'vue';
   import ToggleIcon from './ToggleIcon.vue';
 
   const props = defineProps({
@@ -22,8 +22,9 @@
   const requiredRule = (v) => !!v || 'This field is required';
   const opType = ref(props.schema.type);
   var consoFlag = ref(false);
-  const m_schema = reactive({...props.schema});
-  const formValues = reactive({...props.initialValues});
+  // Set m_schema as a local, mutable copy of the input schema prop
+  var m_schema = reactive(JSON.parse(JSON.stringify(props.schema)));
+  var formValues = reactive({...props.initialValues});
 
   const fieldTypes = computed(() => {
     return m_schema.fields.filter(fld => fld.searchable).map(f => f.type);
@@ -106,8 +107,26 @@
       if (_idx >= 0) m_schema.fields[_idx]['visible'] = false;
       if (typeof(formValues['group']) != 'undefined') formValues['group'] = null;
     } else if (fieldName=='institutions') {
+      // If UNSET exists in options, limit platforms based on chosen inst
+      if (m_schema.unset.length>0) {
+        // get all UNSET platforms for the selected inst
+        m_schema.options.platforms = m_schema.options.platforms.items.filter( plat =>
+          m_schema.unset.filter( opt => opt.inst_id == formValues['institutions'])
+                        .map( p => p.plat_id).includes(plat.id)
+        );
+      }
       let _idx = m_schema.fields.findIndex(f => f.name=='inst_id');
       if (_idx >= 0) formValues['inst_id'] = formValues['institutions'];
+    } else if (fieldName=='platforms') {
+      // If UNSET exists in options, limit institutions based on chosen platform
+      if (m_schema.unset.length>0) {
+        m_schema.options.institutions = m_schema.options.institutions.items.filter( inst =>
+          m_schema.unset.filter( opt => opt.plat_id == formValues['platforms'])
+                        .map( p => p.inst_id).includes(inst.id)
+        );
+      }
+      let _idx = m_schema.fields.findIndex(f => f.name=='prov_id');
+      if (_idx >= 0) formValues['prov_id'] = formValues['platforms'];
     } else if (fieldName=='group') {
       let _idx = m_schema.fields.findIndex(f => f.name=='institution');
       if (_idx >= 0) m_schema.fields[_idx]['visible'] = false;
@@ -122,10 +141,49 @@
   }
 
   function submitForm() {
+    // apply values of institutions and platforms to inst_id and prov_id if present
+    let i_idx = m_schema.fields.findIndex(f => f.name=='inst_id');
+    if (i_idx>0 && typeof(formValues['institutions'])!='undefined' && !Array.isArray(formValues['institutions'])) {
+      formValues['inst_id'] = formValues['institutions'];
+    }
+    let p_idx = m_schema.fields.findIndex(f => f.name=='prov_id');
+    if (p_idx>0 && typeof(formValues['platforms'])!='undefined' && !Array.isArray(formValues['platforms'])) {
+      formValues['prov_id'] = formValues['platforms'];
+    }
     if (formRef.value?.validate()) {
       emit('submit', formValues);
     }
   }
+onMounted(() => {
+  // Preset and/or limit specific select fields
+  m_schema.fields.forEach( (fld) => {
+    if (fld.type == 'select' && typeof(m_schema.options[fld.name])!='undefined' &&
+        typeof(formValues[fld.name])!='undefined') {
+      if (Array.isArray(m_schema.options[fld.name].items) && m_schema.unset.length>0) {
+        // Limit options for insitutions and platforms using unset pairs
+        if (fld.name == 'institutions') {
+          m_schema.options[fld.name].items = props.schema.options[fld.name].items.filter(
+            inst => m_schema.unset.map( p => p.inst_id).includes(inst.id));
+        }
+        if (fld.name == 'platforms') {
+          m_schema.options[fld.name].items = props.schema.options[fld.name].items.filter(
+            plat => m_schema.unset.map( p => p.plat_id).includes(plat.id));
+        }
+        // Preset values if there's only one item in options
+        if (m_schema.options[fld.name].items.length == 1) {
+          formValues[fld.name] = m_schema.options[fld.name].items[0][fld.optVal];
+          // Set inst_id and prov_id explictly for institutions and platforms
+          if (fld.name == 'institutions' && typeof(formValues['inst_id'])!='undefined') {
+            formValues['inst_id'] = formValues['institutions'];
+          }
+          if (fld.name == 'platforms' && typeof(formValues['prov_id'])!='undefined') {
+            formValues['prov_id'] = formValues['platforms'];
+          }
+        }
+      }
+    } 
+  });
+});
 </script>
 
 <template>
@@ -133,8 +191,10 @@
     <v-form v-if="m_schema.fields.length" ref="formRef" @submit.prevent="submitForm">
       <v-container>
         <v-row v-for="field in m_schema.fields" :key="field.name" class="mb-3">
-          <v-col v-if="!field.static && (opType=='Edit' || field.editable) && field.visible" cols="12"
-                 class="d-flex ma-0 pa-0">
+          <v-col v-if="field.visible && (!field.static && (
+                                         (opType=='Edit' && (field.editable || !field.isFilter)) ||
+                                         (opType=='Add'  && m_schema.requiredKeys.includes(field.name))))"
+                 cols="12" class="d-flex ma-0 pa-0">
             <!-- Read-only display -->
             <div v-if="opType!='Add' && (!field.editable || field.renderAsText)">
               <strong>{{ field.label }}:</strong> &nbsp;
@@ -147,7 +207,6 @@
               <ToggleIcon v-model="formValues[field.name]" :toggleable="isToggleEditable(formValues[field.name])"
                           :size="36" @update:modelValue="toggleChanged(field.name)" />
             </div>
-                        
             <!-- Password inputs -->
             <v-text-field v-else-if="field.type==='password'" v-model="formValues[field.name]" :label="field.label"
                           :type="pw_show ? 'text' : 'password'" :append-icon="pw_show ? 'mdi-eye' : 'mdi-eye-off'"
@@ -165,16 +224,16 @@
 
             <!-- Select inputs -->
             <v-select v-else-if="field.type==='select'" v-model="formValues[field.name]" :label="field.label"
-                      :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
+                      :items="m_schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
                       :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined"
                       :rules="field.required ? [requiredRule] : []" density="compact"
                       @update:modelValue="selectChanged(field.name)" />
             <v-select v-else-if="field.type==='selectObj'" v-model="formValues[field.name]" :label="field.label"
-                      :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
+                      :items="m_schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal" 
                       :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined"
                       return-object :rules="field.required ? [requiredRule] : []" density="compact"/>
             <v-combobox v-else-if="field.type==='mselect'" v-model="formValues[field.name]" :label="field.label"
-                      :items="schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal"
+                      :items="m_schema.options[field.name].items" :item-title="field.optTxt" :item-value="field.optVal"
                       :prepend-icon="field.icon" :hint="field.helperText" persistent-hint variant="outlined" multiple
                       :rules="field.required ? [requiredRule] : []" density="compact"/>
 
