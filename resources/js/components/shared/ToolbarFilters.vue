d1c81863f6973451a00d70c52d908617c99690db
Scott Ross <lsr2912@gmail.com>
2026-02-11 17:06:53 -0500

    Add support for mtext filter type; change to handle filters as a prop (instead of defineModel) since this only renders a single row now. Emits changed to return key and value upstream

--- resources/js/components/shared/ToolbarFilters.vue
+++ resources/js/components/shared/ToolbarFilters.vue
@@ -1,29 +1,39 @@
 <!-- shared/ToolbarFilters -->
 <script setup>
-  const filters = defineModel({type: Object, required: true});
+  const props = defineProps({
+    filters: { type: Object, required: true },
+  });
+  var m_filters = {...props.filters};
   const emit = defineEmits(['setFilter']);
+  function handleFilter(filt) {
+    emit('setFilter', {key: filt.name, value: filt.value});
+  }
 </script>
 
 <template>
-  <!-- <v-col v-for="(filter,idx) in filters" :key="idx" :cols="(typeof(filter.cols)=='undefined') ? 2 : filter.cols"> -->
-  <v-col v-for="(filter,idx) in filters" :key="idx" cols="2">
+  <v-col v-for="(filter,idx) in m_filters" :key="idx" class="d-flex pl-2" cols="2">
 
     <!-- AutoComplete - multiple select -->
-    <v-autocomplete v-if="filter.show && filter.type=='mselect'" v-model="filters[filter.name].value" :label="filter.label"
-        multiple :items="filter.items" :item-title="filter.txt" :item-value="filter.val"
-        @update:modelValue="$emit('setFilter')" />
+    <v-autocomplete v-if="filter.show && filter.type=='mselect'" v-model="m_filters[filter.name].value" :label="filter.label"
+        multiple :items="filter.items" :item-title="filter.txt" :item-value="filter.val" density="compact"
+        @update:modelValue="handleFilter(filter)" />
 
     <!-- AutoComplete - single item select -->
-    <v-autocomplete v-if="filter.show && filter.type=='select'" v-model="filters[filter.name].value" :label="filter.label"
-        :items="filter.items" :item-title="filter.txt" :item-value="filter.val"
-        @update:modelValue="$emit('setFilter')" />
+    <v-autocomplete v-if="filter.show && filter.type=='select'" v-model="m_filters[filter.name].value" :label="filter.label"
+        :items="filter.items" :item-title="filter.txt" :item-value="filter.val" density="compact"
+        @update:modelValue="handleFilter(filter)" />
 
     <!-- AutoComplete - return single object -- needed anywhere??
-    <v-autocomplete v-if="filter.type=='select'" v-model="filters[filter.name].value" :label="filter.label"
+    <v-autocomplete v-if="filter.type=='select'" v-model="m_filters[filter.name].value" :label="filter.label"
         return-object :items="filter.items" :item-title="filter.txt" :item-value="filter.val" />
     -->
+
+    <!-- Return array of Scalar values -->
+    <v-select v-if="filter.show && filter.type=='mtext'" v-model="m_filters[filter.name].value" :label="filter.label"
+        :items="filter.items" @update:modelValue="handleFilter(filter)" multiple density="compact" />
+
     <!-- Return a Single Scalar value -->
-    <v-select v-if="filter.show && filter.type=='text'" v-model="filters[filter.name].value" :label="filter.label"
-        :items="filter.items" @update:modelValue="$emit('setFilter')" />
+    <v-select v-if="filter.show && filter.type=='text'" v-model="m_filters[filter.name].value" :label="filter.label"
+        :items="filter.items" @update:modelValue="handleFilter(filter)" density="compact" />
   </v-col>
 </template>
