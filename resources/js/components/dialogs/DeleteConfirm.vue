b5f85b55d718ded0891f143a939b5730b81959b9
Scott Ross <lsr2912@gmail.com>
2026-02-16 12:49:09 -0500

    Pur confirmation and details dialog templates into their own components

---
+++ resources/js/components/dialogs/DeleteConfirm.vue
@@ -0,0 +1,37 @@
+<!-- components/dialogs/DeleteConfirm.vue -->
+<script setup>
+  const props = defineProps({
+    dataset: { type:Object, required: true },
+    item: { type:Object, required: true },
+  });
+  const emit = defineEmits(['close', 'confirm']);
+</script>
+<template>
+  <v-card>
+    <v-card-title class="text-indigo-darken-2 pa-6 d-flex justify-space-between align-center">
+      <span>Confirm Delete</span>
+      <v-tooltip text="Cancel" location="bottom">
+        <template #activator="{ props }">
+          <v-btn icon variant="outlined" class="close-btn" v-bind="props" @click="$emit('close')">
+            <v-icon size="18">mdi-close</v-icon>
+          </v-btn>
+        </template>
+      </v-tooltip>
+    </v-card-title>
+    <v-card-text>
+      <p>&nbsp;</p>
+      <p>
+        <strong>You are about to delete {{ item.name }} from {{ props.dataset }}</strong>
+      </p>
+      <p>&nbsp;</p>
+      <h3> Are you Sure?</h3>
+      <p>&nbsp;</p>
+      <v-row>
+        <v-col cols="12" class="text-left">
+          <v-btn color="primary" @click="$emit('confirm')">Yes, Delete it</v-btn>
+          <v-btn variant="text" class="ml-2" @click="$emit('close')">Cancel</v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
---
+++ resources/js/components/dialogs/ErrorDetails.vue
@@ -0,0 +1,65 @@
+<!-- components/dialogs/ErrorDetails.vue -->
+<script setup>
+  const props = defineProps({
+    error: { type:Object, required: true },
+  });
+  const emit = defineEmits(['close']);
+  function goCOUNTER(errorId, urlbase) {
+    let features = "";
+    let _url = urlbase;
+    if ('fragmentDirective' in document) {
+      _url += "#:~:text="+errorId.toString();
+      features += "noopener";
+    }
+    window.open(_url, "_blank", features);
+  }
+</script>
+<template>
+  <v-card>
+    <v-card-title class="text-indigo-darken-2 pa-6 d-flex justify-space-between align-center">
+      <span v-if="error.id<9000">COUNTER Error : {{ error.id }}</span>
+      <span v-else>CC-Plus Error : {{ error.id }}</span>
+      <v-tooltip text="Cancel" location="bottom">
+        <template #activator="{ props }">
+          <v-btn icon variant="outlined" class="close-btn" v-bind="props" @click="$emit('close')">
+            <v-icon size="18">mdi-close</v-icon>
+          </v-btn>
+        </template>
+      </v-tooltip>
+    </v-card-title>
+    <v-card-text>
+      <v-container grid-list-md>
+        <v-row class="d-flex mb-2" no-gutters>
+          <v-col class="d-flex">{{ error.message }}</v-col>
+        </v-row>
+        <v-row class="d-flex mb-1" no-gutters>
+          <v-col class="d-flex pa-0" cols="3"><strong>Details:</strong></v-col>
+          <v-col class="d-flex px-4" cols="9">{{ error.explanation }}</v-col>
+        </v-row>
+        <v-row class="d-flex mb-1" no-gutters>
+          <v-col class="d-flex pa-0" cols="3"><strong>Suggestion:</strong></v-col>
+          <v-col class="d-flex px-4" cols="9">{{ error.suggestion }}</v-col>
+        </v-row>
+        <v-row v-if="error.detail.length>0" class="d-flex mb-1" no-gutters>
+          <v-col class="d-flex pa-0" cols="3">
+            <strong>{{ error.process_step }} Step:</strong>
+          </v-col>
+          <v-col class="d-flex px-4" cols="9">{{ error.detail }}</v-col>
+        </v-row>
+        <v-row v-if="error.help_url.length>0" class="d-flex mb-1" no-gutters>
+          <v-col class="d-flex pa-0" cols="3"><strong>Help URL:</strong></v-col>
+          <v-col class="d-flex px-4" cols="9">
+            <a :href="error.help_url" target="_blank">{{ error.help_url }}</a>
+          </v-col>
+        </v-row>
+        <v-row v-if="error.id<9000" class="d-flex mb-1" no-gutters>
+          <v-col class="d-flex pa-0">
+            <a href="#" @click="goCOUNTER(error.id, error.counter_url)">
+              Open COUNTER Details <v-icon>mdi-open-in-new</v-icon>
+            </a>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
