0df91044c7901dc14e06dc688a5d52823268b437
Scott Ross <lsr2912@gmail.com>
2026-02-16 13:00:39 -0500

    Restore reactivity to filters.. copying to a mutable broke it.

--- resources/js/components/shared/ToolbarFilters.vue
+++ resources/js/components/shared/ToolbarFilters.vue
@@ -3,7 +3,6 @@
   const props = defineProps({
     filters: { type: Object, required: true },
   });
-  var m_filters = {...props.filters};
   const emit = defineEmits(['setFilter']);
   function handleFilter(filt) {
     emit('setFilter', {key: filt.name, value: filt.value});
@@ -11,15 +10,15 @@
 </script>
 
 <template>
-  <v-col v-for="(filter,idx) in m_filters" :key="idx" class="d-flex pl-2" cols="2">
+  <v-col v-for="(filter,idx) in props.filters" :key="idx" class="d-flex pl-2" cols="2">
 
     <!-- AutoComplete - multiple select -->
-    <v-autocomplete v-if="filter.show && filter.type=='mselect'" v-model="m_filters[filter.name].value" :label="filter.label"
+    <v-autocomplete v-if="filter.show && filter.type=='mselect'" v-model="props.filters[filter.name].value" :label="filter.label"
         multiple :items="filter.items" :item-title="filter.txt" :item-value="filter.val" density="compact"
         @update:modelValue="handleFilter(filter)" />
 
     <!-- AutoComplete - single item select -->
-    <v-autocomplete v-if="filter.show && filter.type=='select'" v-model="m_filters[filter.name].value" :label="filter.label"
+    <v-autocomplete v-if="filter.show && filter.type=='select'" v-model="props.filters[filter.name].value" :label="filter.label"
         :items="filter.items" :item-title="filter.txt" :item-value="filter.val" density="compact"
         @update:modelValue="handleFilter(filter)" />
 
@@ -29,11 +28,11 @@
     -->
 
     <!-- Return array of Scalar values -->
-    <v-select v-if="filter.show && filter.type=='mtext'" v-model="m_filters[filter.name].value" :label="filter.label"
+    <v-select v-if="filter.show && filter.type=='mtext'" v-model="props.filters[filter.name].value" :label="filter.label"
         :items="filter.items" @update:modelValue="handleFilter(filter)" multiple density="compact" />
 
     <!-- Return a Single Scalar value -->
-    <v-select v-if="filter.show && filter.type=='text'" v-model="m_filters[filter.name].value" :label="filter.label"
+    <v-select v-if="filter.show && filter.type=='text'" v-model="props.filters[filter.name].value" :label="filter.label"
         :items="filter.items" @update:modelValue="handleFilter(filter)" density="compact" />
   </v-col>
 </template>
