ee474a9af8ce7e4b6f5561445fb15acd1ee4145c
Scott Ross <lsr2912@gmail.com>
2026-02-20 16:07:32 -0500

    Add wrapper for CredentialsAudit datatable

---
+++ resources/js/components/tables/CredentialsAudit.vue
@@ -0,0 +1,9 @@
+<!-- tables/CredentialsAudit.vue -->
+<script setup>
+  import DatasetViewer from '../shared/DatasetViewer.vue';
+  const emit = defineEmits(['updateConso']);
+</script>
+
+<template>
+  <DatasetViewer datasetKey="audit" @updateConso="$emit('updateConso', $event)" />
+</template>
