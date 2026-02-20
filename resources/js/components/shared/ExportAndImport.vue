c037fd22a98a9a38f48279acc1384c3df7dd5f67
Scott Ross <lsr2912@gmail.com>
2026-02-20 16:26:02 -0500

    Add props for exportOnly and hideExport to control button visibility

--- resources/js/components/shared/ExportAndImport.vue
+++ resources/js/components/shared/ExportAndImport.vue
@@ -3,29 +3,32 @@
 <script setup>
   const props = defineProps({
     showRefresh: { type: Boolean, required: true },
+    exportOnly: { type: Boolean, required: true },
+    hideExport: { type: Boolean, required: true },
   });
   defineEmits(['export', 'import', 'add', 'refresh']);
 </script>
 
 <template>
   <!-- Refresh Button -->
-  <v-col v-if='props.showRefresh' cols="auto">
+  <v-col v-if='props.showRefresh && !props.exportOnly' cols="auto">
     <v-btn color="primary" @click="$emit('refresh')">Full Registry Refresh</v-btn>
   </v-col>
   <!-- Add Button -->
-  <v-col cols="auto">
+  <v-col v-if='!props.exportOnly' cols="auto">
     <v-btn color="primary" @click="$emit('add')">
     <v-icon start>mdi-plus</v-icon>
       Add
     </v-btn>
   </v-col>
-  <v-col cols="auto">
+  <v-col v-if='!props.hideExport' cols="auto">
     <v-btn color="secondary" @click="$emit('export')">
     <v-icon start>mdi-export</v-icon>
       Export
     </v-btn>
   </v-col>
-  <v-col cols="auto">
+  <!-- hideExport also hides import -->
+  <v-col v-if='!props.exportOnly && !props.hideExport' cols="auto">
     <v-btn color="secondary" @click="$emit('import')">
     <v-icon start>mdi-upload</v-icon>
       Import
