e4ac4b28def94c7339d9c6ba8917d005d788b29e
Scott Ross <lsr2912@gmail.com>
2026-02-25 16:40:25 -0500

    Updates to spacing/dividers/icons, and making inputs look+feel more alike

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -65,41 +65,43 @@ onMounted(() => {
 </script>
 
 <template>
-<v-card class="login-card" elevation="4" max-width="420">
-  <div class="text-center mb-6">
-    <img src="/images/CC_Plus_Logo.png" alt="CC Plus" height="50" width="103" class="login-logo" />
-  </div>
-  <form method="POST" action="" @submit.prevent="formSubmit" class="login-form">
-    <div class="mb-4">
-      <template v-if="preset_conso.length > 0">
-        <h5 class="text-center">Logging into {{ preset_conso }}</h5>
-      </template>
-      <template v-else>
-      <v-select :items='consortia' v-model='consortium' label="Consortium" 
-                item-title="name" return-object autofocus density="compact" />
-      </template>
+  <v-card class="login-card" elevation="4" max-width="420">
+    <div class="text-center mb-4">
+      <img src="/images/CC_Plus_Logo.png" alt="CC Plus" height="50" width="103" class="login-logo my-4" />
     </div>
-    <!-- Email -->
-    <v-text-field id="email" type="text" name="email" label="Email" class="mb-4" required
-          v-model="email" placeholder="Email address" density="compact" clearable />
-    <!-- Password -->
-    <v-text-field id="password" :type="pw_show ? 'text' : 'password'" name="password" label="Password"
-                  :append-icon="pw_show ? 'mdi-eye' : 'mdi-eye-off'" @click:append="pw_show = !pw_show"
-                  v-model="password" placeholder="Password" density="compact" required  class="mb-6"/>
-    <!-- Buttons -->
-    <div class="d-flex justify-space-between">
-      <v-btn size="small" class="login-primary" type="submit" color="primary" variant="elevated">
-        Login
-      </v-btn>
-      <v-btn size="small" variant="text" @click="triggerApiCall('/forgotPassForm')">
-        Forgot Your Password
-      </v-btn>
-    </div>
-  </form>
+    <div class="section-divider mb-6"></div>
+    <form method="POST" action="" @submit.prevent="formSubmit" class="login-form">
+      <div class="mb-4">
+        <template v-if="preset_conso.length > 0">
+          <h5 class="text-center">Logging into {{ preset_conso }}</h5>
+        </template>
+        <template v-else>
+        <v-select v-model='consortium' :items='consortia' item-title="name" return-object label="Consortium" 
+                  variant="outlined" density="comfortable" clearable hide-details="auto" />
+        </template>
+      </div>
+      <!-- Email -->
+      <v-text-field id="email" v-model="email" type="text" name="email" label="Email" placeholder="Email address"
+                    variant="outlined" density="comfortable" clearable hide-details="auto" class="mb-4" required />
+      <!-- Password -->
+      <v-text-field id="password" v-model="password" :type="pw_show ? 'text' : 'password'" name="password" label="Password"
+                    placeholder="Password" variant="outlined" density="comfortable" clearable hide-details="auto"
+                    :append-inner-icon="pw_show ? 'mdi-eye' : 'mdi-eye-off'" @click:append-inner="pw_show = !pw_show"
+                    class="mb-6" required />
+      <!-- Buttons -->
+      <div class="d-flex justify-space-between">
+        <v-btn size="small" class="login-primary" type="submit" color="primary" variant="elevated">
+          Login
+        </v-btn>
+        <v-btn size="small" variant="text" @click="triggerApiCall('/forgotPassForm')">
+          Forgot Your Password
+        </v-btn>
+      </div>
+    </form>
+    <div class="section-divider mt-6"></div>
     <div v-if="errorMessage" class="login-notices mt-4">
       <span class="redNotice">{{ errorMessage }}</span>
     </div>
-
     <div v-else-if="successMessage" class="login-notices mt-4">
       <span class="greenNotice">{{ successMessage }}</span>
     </div>
@@ -113,6 +115,10 @@ onMounted(() => {
 .login-logo {
   display: block;
   margin: 0 auto;
+}
+.section-divider {
+  border-bottom: 1px solid #d3d3d3;
+  width: 100%;
 }
 .login-notices {
   text-align: center;
