068f277b3bbf683611835777ddf4e9f66cc1ceb5
Scott Ross <lsr2912@gmail.com>
2026-04-10 18:03:33 -0400

    Remove unneeded components

--- resources/js/components/tables/SavedReports.vue
+++
@@ -1,9 +0,0 @@
-<!-- tables/SavedReports.vue -->
-<script setup>
-  import DatasetViewer from '../shared/DatasetViewer.vue';
-  const emit = defineEmits(['updateConso']);
-</script>
-
-<template>
-  <DatasetViewer datasetKey="savedreports" @updateConso="$emit('updateConso', $event)" />
-</template>
