074c5023aba0c58d1c5ddcf702e25e4ec9348c52
Scott Ross <lsr2912@gmail.com>
2026-04-10 17:58:51 -0400

    Update class and migration to remove master_id, add rpt_only, and rename inherited_fields to fields; update master()  to return itself if it's a master; simplify filterBy(); Add new static public function formattedReports() to send back all of the current user's reports in a predictable structure - also returns a single report if given an ID

--- app/Models/SavedReport.php
+++ app/Models/SavedReport.php
@@ -21,8 +21,8 @@ class SavedReport extends Model
    * @var array
    */
     protected $fillable = [
-      'title', 'user_id', 'date_range', 'master_id', 'report_id', 'ym_from', 'ym_to', 'inherited_fields', 'filters',
-      'format', 'exclude_zeros'
+      'title', 'user_id', 'date_range', 'ym_from', 'ym_to', 'report_id', 'fields', 'filters',
+      'format', 'exclude_zeros', 'rpt_only'
     ];
 
     public function user()
@@ -37,7 +37,8 @@ class SavedReport extends Model
 
     public function master()
     {
-        return $this->belongsTo('App\Models\Report', 'master_id');
+        $theReport = $this->report()->with('parent')->first();
+        return ($theReport->parent) ? $theReport->parent : $this->report();
     }
 
     public function canManage()
@@ -80,13 +81,15 @@ class SavedReport extends Model
     }
 
     // Return a filters array that matches the vue-datastore filter_by object
+    // before calling this, the caller really should lazy-load SavedReport
+    //  ->with('master','master.reportFields','master.reportFields.reportFilter')
     public function filterBy()
     {
         $return_filters = array('report_id' => $this->master->id);
-        $return_filters['toYM'] = date("Y-m", strtotime("-1 month"));
-        $return_filters['fromYM'] = date("Y-m", strtotime("-" . $this->months . " months"));
+        $return_filters['toYM'] = $this->ym_to;
+        $return_filters['fromYM'] = $this->ym_from;
 
-        // Define the filter presets for this SavedReport
+        // Define the filter limits for this SavedReport
         $my_filters = $this->parsedFilters();
         if (count($my_filters) > 0) {
             foreach ($my_filters as $key => $value) {
@@ -104,11 +107,7 @@ class SavedReport extends Model
             if ($field->reportFilter) {
                 $_col = $field->reportFilter->report_column;
                 if (!isset($return_filters[$_col])) {
-                    if ($_col == 'inst_id' || $_col == 'prov_id' || $_col == 'plat_id' || $_col == 'yop') {
-                        $return_filters[$_col] = [];
-                    } else {
-                        $return_filters[$_col] = 0;
-                    }
+                    $return_filters[$_col] = ($_col == 'institutiongroup_id') ? 0 : [];
                 }
             }
         }
@@ -116,5 +115,121 @@ class SavedReport extends Model
             $return_filters['institutiongroup_id'] = 0;
         }
         return $return_filters;
+    }
+
+   /**
+     * Return a structured array of saved reports for the current user, or for a single
+     * SavedReport record given an ID
+     *
+     * @param  SavedReport $id
+     * @return Array
+     */
+    public static function formattedReports($id = null)
+    {
+        $thisUser = auth()->user();
+        $report_data = array();
+
+        // Get SavedReport record(s)
+        $saved_reports = SavedReport::with('master','master.reportFields','master.reportFields.reportFilter','report')
+                                    ->when(!is_null($id), function ($qry) use ($id) {
+                                        return $qry->where('id',$id);
+                                    })
+                                    ->when(is_null($id), function ($qry) use ($thisUser) {
+                                        return $qry->where('user_id', $thisUser->id);
+                                    })->get();
+        if (!$saved_reports) {
+            return $report_data;
+        }
+
+        // Get the report filters
+        $all_filters = ReportFilter::get(['id','table_name']);
+
+        // Get names and IDs for providers, institutions, platforms, and groups
+        $all_insts = Institution::where('id', '>', 1)->get(['id','name']);
+        $all_provs = GlobalProvider::get(['id','name']);
+        $all_plats = Platform::get(['id','name']);
+        $all_groups = InstitutionGroup::get(['id','name']);
+
+        // Build the output data array
+        foreach ($saved_reports as $report) {
+            $master_id = ($report->report->parent_id>0) ? $report->report->parent_id : $report->report->id;
+            $last_harvest = HarvestLog::where('report_id', $master_id)->max('yearmon');
+            $data = array('id' => $report->id, 'title' => $report->title,
+                          'report_id' => $report->report_id, 'master_id' => $master_id, 'format' => $report->format,
+                          'report_legend' => $report->report->legend, 'report_name' => $report->report->name,
+                          'exclude_zeros' => $report->exclude_zeros, 'date_range' => $report->date_range,
+                          'ym_from' => $report->ym_from, 'ym_to' => $report->ym_to, 'can_delete' => true,
+                          'last_harvest' => $last_harvest, 'updated_at' => $report->updated_at);
+
+            // Setup array of filter-data to be added to the report
+            $filter_data = $report->filterBy();
+
+            // Get master fields for $report->fields and tack on filter relationship
+            $fields = $report->master->reportFields->whereIn('id', preg_split('/,/', $report->fields));
+            $fields->load('reportFilter');
+            $data['report_fields'] = array();
+            foreach($fields as $field) {
+                $rec = array('id'=>$field->id, 'name'=>$field->legend, 'qry_as'=>$field->qry_as,
+                             'is_metric'=>$field->is_metric, 'metric_type'=>$field->metric_type, 'names'=>'All');
+                if ($field->reportFilter) {
+                    if ($field->qry_as == 'institution' &&
+                        (count($filter_data['inst_id']) > 0 || $filter_data['institutiongroup_id'] > 0)) {
+                        if ($filter_data['institutiongroup_id'] > 0) {
+                            $rec['name'] = 'Institution Group';
+                            $_group = $all_groups->where('id', $filter_data['institutiongroup_id'])->first();
+                            $rec['names'] = ($_group) ? $_group->name : "";
+                        } else {
+                            $rec['names'] = '';
+                            foreach ($filter_data['inst_id'] as $val) {
+                                $_inst = $all_insts->where('id', $val)->first();
+                                if ($_inst) {
+                                    $rec['names'] .= $_inst->name . ', ';
+                                }
+                            }
+                        }
+                        $rec['names'] = rtrim(trim($rec['names']), ',');
+                    } elseif ($field->qry_as == 'provider' && count($filter_data['prov_id']) > 0) {
+                        $rec['names'] = '';
+                        foreach ($filter_data['prov_id'] as $val) {
+                            $_prov = $all_provs->where('id', $val)->first();
+                            if ($_prov) {
+                                $rec['names'] .= $_prov->name . ', ';
+                            }
+                        }
+                        $rec['names'] = rtrim(trim($rec['names']), ',');
+                    } elseif ($field->qry_as == 'platform' && count($filter_data['plat_id']) > 0) {
+                        $rec['names'] = '';
+                        foreach ($filter_data['plat_id'] as $val) {
+                            $_plat = $all_plats->where('id', $val)->first();
+                            if ($_plat) {
+                                $rec['names'] .= $_plat->name . ', ';
+                            }
+                        }
+                        $rec['names'] = rtrim(trim($rec['names']), ',');
+                    } elseif ($field->qry_as == 'yop' && count($filter_data['yop']) > 0) {
+                        $rec['names'] = $filter_data['yop'][0] . ' to ' . $filter_data['yop'][1];
+                    } elseif (!in_array($field->qry_as, ['institution','provider','platform'])) {
+                        if (isset($filter_data[$field->reportFilter->report_column])) {
+                            $filter_id = $filter_data[$field->reportFilter->report_column];
+                            if ($field->reportFilter->model) {
+                                $rec['names'] = $field->reportFilter->model::where('id', $filter_id)->value('name');
+                            }
+                        }
+                    }
+                }
+                // Filter-handling
+                $rec['limit'] = [];
+                $rec['column'] = null;
+                if ($field->reportFilter) {
+                    $rec['column'] = $field->reportFilter->report_column;
+                    // If a filter is set on this field's column, add it as 'limit'
+                    $rec['limit'] = (in_array($field->reportFilter->report_column,array_keys($filter_data)))
+                                     ? $filter_data[$field->reportFilter->report_column] : [];
+                }
+                $data['report_fields'][] = $rec;
+            }
+            $report_data[] = $data;
+        }
+        return (is_null($id)) ? $report_data : $report_data[0];
     }
 }
--- database/migrations/con_template/2019_07_18_044001_create_savedreports_table.php
+++ database/migrations/con_template/2019_07_18_044001_create_savedreports_table.php
@@ -22,16 +22,15 @@ class CreateSavedReportsTable extends Migration
             $table->string('date_range', 12)->default('Custom');
             $table->string('ym_from', 7)->nullable();
             $table->string('ym_to', 7)->nullable();
-            $table->unsignedInteger('master_id');
             $table->unsignedInteger('report_id');
-            $table->string('inherited_fields')->nullable();
+            $table->string('fields')->nullable();
             $table->string('filters')->nullable();
             $table->string('format', 7)->default('Compact');
             $table->boolean('exclude_zeros')->default(1);
+            $table->boolean('rpt_only')->default(0);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('master_id')->references('id')->on($global_db . '.reports');
         });
     }
 
